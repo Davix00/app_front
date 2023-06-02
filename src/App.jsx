29adfc6a6d5da30cec27fece04b7ef4ca6797b9e
David Galaviz <galavizgdavid899@gmai.com>
2023-06-02 10:56:15 -0600

    agregando login

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,19 @@
 import { Fragment, useState } from 'react'
-import Menu from '../components2/menu';
-import Aside from '../components2/Aside';
-import Content from '../components2/Content';
-import Footer from '../components2/Footer';
+// import Menu from '../components2/menu';
+// import Aside from '../components2/Aside';
+// import Content from '../components2/Content';
+// import Footer from '../components2/Footer';
+import Login from '../components2/Login';
 
 function App() {
   return (
     <Fragment>
-        <Menu />
+        {/* <Menu />
         <Aside />
         <Content />
-        <Footer />
+        <Footer /> */
+        <Login/>}
+        
    </Fragment>
    )
 }
